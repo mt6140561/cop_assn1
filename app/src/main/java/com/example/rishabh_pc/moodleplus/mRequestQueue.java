0d9a33b10530190cd08adad4b1b768a94fbd7b25
mt6140561 <rish.mahe@gmail.com>
2016-02-17 23:20:13 +0530

    login is done

---
+++ app/src/main/java/com/example/rishabh_pc/moodleplus/mRequestQueue.java
@@ -0,0 +1,13 @@
+package com.example.rishabh_pc.moodleplus;
+
+import com.android.volley.RequestQueue;
+
+/**
+ * Created by Rishab-pc on 17-Feb-16.
+ */
+public class mRequestQueue{
+    public RequestQueue req;
+    public mRequestQueue() {
+
+    }
+}
